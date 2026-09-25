aae9216032297e3a9f57304d7932f872f9e7bfce
garrygit config --user.email newby1979@gmail.com <69111078+090221979@users.noreply.github.com>
2026-09-26 00:06:48 +0100

    added css

--- project-notebook_frontend/src/App.jsx
+++ project-notebook_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Note from './components/Note';
 import Notification from './components/Notification';
 import noteService from './services/notes';
+import './index.css';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
